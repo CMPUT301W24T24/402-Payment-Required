21c88420584ee3d3e8ef4a6be85ea268cf2e4da4
Damon Grant <dcgrant@ualberta.ca>
2024-02-21 21:20:55 -0700

    Created Event, EventList, UserList classes

---
+++ src/app/src/main/java/com/example/qrcheckin/EventList.java
@@ -0,0 +1,52 @@
+package com.example.qrcheckin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class which keeps track of a list of event objects (all the events on the app)
+ */
+public class EventList {
+    private List<Event> events = new ArrayList<>();
+
+    /**
+     * Adds a new event to the list if it does not exist
+     * @param event the new event
+     */
+    public void add(Event event) {
+        if (events.contains(event)) {
+            throw new IllegalArgumentException("This event already exists.");
+        }
+        events.add(event);
+    }
+
+    /**
+     * Checks if an event exists or not
+     * @param event the event that is being checked
+     * @return
+     * true if the event exists, false if it does not
+     */
+    public Boolean hasEvent(Event event) {
+        return events.contains(event);
+    }
+
+    /**
+     * Removes an event from the list if it exists
+     * @param event the event being removed
+     */
+    public void removeEvent(Event event) {
+        if (!hasEvent(event)) {
+            throw new IllegalArgumentException("This event already does not exist.");
+        }
+        events.remove(event);
+    }
+
+    /**
+     * Returns the amount of events
+     * @return the size of the event list
+     */
+    public int countEvents() {
+        return events.size();
+    }
+
+}
