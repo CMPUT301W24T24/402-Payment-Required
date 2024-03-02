648f95dde27003b6af028fe658b638113b46ad7f
Brian <shihhsu1@ualberta.ca>
2024-03-02 15:58:53 -0700

    Updated Event object and constructor

--- src/app/src/main/java/com/example/qrcheckin/core/Event.java
+++ src/app/src/main/java/com/example/qrcheckin/core/Event.java
@@ -1,5 +1,6 @@
 package com.example.qrcheckin.core;
 
+import java.util.Date;
 import java.util.Objects;
 
 /**
@@ -8,21 +9,72 @@ import java.util.Objects;
 public class Event {
 
     private String id;
+    private User host;
     private String name;
-    private User owner;
+    private String description;
+    private String posterRef;
+    private Date time;
+    private String location;
+    private Double locationGeoLat;
+    private Double locationGeoLong;
+    private String checkinId;
+    private String checkinRq;
+    private String promoteId;
+    private String promoteRq;
+    private Boolean geo;
+    private Integer limit;
     private UserList attendees;
 
     /**
-     * The constructor for the event class
+     * A constructor for the event object
      * @param id the id of the event
+     * @param host the user hosting the event
      * @param name the name of the event
-     * @param owner who is hosting the event
-     * @param attendees a list of those attending the event
+     * @param description the description of the event
+     * @param posterRef the reference to the poster of the event
+     * @param time the time of the event
+     * @param location the location of the event
+     * @param locationGeoLat the latitude of the location
+     * @param locationGeoLong the longitude of the location
+     * @param checkinId the id of the checkin
+     * @param promoteId the id of the promotion
+     * @param geo the boolean value of the location
+     * @param limit the limit of the event
+     * @param attendees the list of users attending the event
      */
-    public Event(String id, String name, User owner, UserList attendees) {
+    public Event(String id, User host, String name, String description, String posterRef, Date time, String location, Double locationGeoLat, Double locationGeoLong, String checkinId, String promoteId, Boolean geo, Integer limit, UserList attendees) {
         this.id = id;
+        this.host = host;
         this.name = name;
-        this.owner = owner;
+        this.description = description;
+        this.posterRef = posterRef;
+        this.time = time;
+        this.location = location;
+        this.locationGeoLat = locationGeoLat;
+        this.locationGeoLong = locationGeoLong;
+        this.checkinId = checkinId;
+        this.promoteId = promoteId;
+        this.geo = geo;
+        this.limit = limit;
+        this.attendees = attendees;
+    }
+
+    public Event(String id, User host, String name, String description, String posterRef, Date time, String location, Double locationGeoLat, Double locationGeoLong, String checkinId, String checkinRq, String promoteId, String promoteRq, Boolean geo, Integer limit, UserList attendees) {
+        this.id = id;
+        this.host = host;
+        this.name = name;
+        this.description = description;
+        this.posterRef = posterRef;
+        this.time = time;
+        this.location = location;
+        this.locationGeoLat = locationGeoLat;
+        this.locationGeoLong = locationGeoLong;
+        this.checkinId = checkinId;
+        this.checkinRq = checkinRq;
+        this.promoteId = promoteId;
+        this.promoteRq = promoteRq;
+        this.geo = geo;
+        this.limit = limit;
         this.attendees = attendees;
     }
 
@@ -57,7 +109,7 @@ public class Event {
      * User who is hosting the event
      */
     public User getOwner() {
-        return owner;
+        return host;
     }
 
     /**
@@ -65,7 +117,7 @@ public class Event {
      * @param owner the User hosting the event
      */
     public void setOwner(User owner) {
-        this.owner = owner;
+        this.host = owner;
     }
 
     /**
