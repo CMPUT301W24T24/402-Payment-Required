068acde70b987a5cff522039380d2a3d614b3fc9
Zumzu <74483517+Zumzu@users.noreply.github.com>
2024-03-02 17:04:12 -0700

    Added untested checkIn function to event

--- src/app/src/main/java/com/example/qrcheckin/core/Event.java
+++ src/app/src/main/java/com/example/qrcheckin/core/Event.java
@@ -67,8 +67,26 @@ public class Event {
         this.distanceLimit=distanceLimit;
     }
 
-    public void checkIn(User user, Double latitude, Double longitude){
-
+    /**
+     * CheckIn attempts to check in a user, and if required checks their location against the events
+     * @param user User who wants to check in to event
+     * @param latitude Latitude of user who wants to check into event
+     * @param longitude Longitude of user who wants to check into event
+     * @return True if user successfully checked in, false otherwise
+     */
+    public boolean checkIn(User user, Double latitude, Double longitude){
+        if(!usesGeolocation()) {
+            (new Database()).checkIn(user,this);
+            if(!attendees.hasUser(user))
+                attendees.add(user);
+            return true;//success
+        }else if(haversine(latitude,longitude,this.latitude,this.longitude) < distanceLimit){
+            (new Database()).checkInWithGeo(user,this,latitude,longitude);
+            if(!attendees.hasUser(user))
+                attendees.add(user);
+            return true;//success
+        }
+        return false;//failed to check in
     }
 
     /** Simple haversine implementation to covert latitude longitude pairs to a distance in meters, see https://www.movable-type.co.uk/scripts/latlong.html
@@ -76,10 +94,11 @@ public class Event {
      */
     public double haversine(Double lat1,Double lon1, Double lat2,Double lon2){
         final int R = 6371000;//radius of earth in meters
-        Double latDistSin = Math.sin((lat2-lat1)*Math.PI/360);
-        Double lonDistSin = Math.sin((lon2-lon1)*Math.PI/360);
-        Double a = latDistSin*latDistSin + Math.cos(lat1*Math.PI/180)*Math.cos(lat2*Math.PI/180) * lonDistSin*lonDistSin;
-        Double distance = R*2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        Double latDistSin = Math.sin(Math.toRadians(lat2-lat1)/2);
+        Double lonDistSin = Math.sin(Math.toRadians(lon2-lon1)/2);
+        Double a = latDistSin*latDistSin + lonDistSin*lonDistSin*Math.cos(lat1*Math.PI/180)*Math.cos(lat2*Math.PI/180);
+        Double c=2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        Double distance = R*c;
         return distance;
     }
 
