26955c90a80668efaf6c27ac2c48f2f55d2a39fe
Damon Grant <dcgrant@ualberta.ca>
2024-03-17 17:24:15 -0600

    Can select images and upload to firestore

--- src/app/src/main/java/com/example/qrcheckin/user/profile/ProfileFragment.java
+++ src/app/src/main/java/com/example/qrcheckin/user/profile/ProfileFragment.java
@@ -8,6 +8,9 @@ import static com.google.common.reflect.Reflection.getPackageName;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +29,7 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.qrcheckin.QRCheckInApplication;
 import com.example.qrcheckin.R;
+import com.example.qrcheckin.core.Database;
 import com.example.qrcheckin.core.User;
 import com.example.qrcheckin.databinding.FragmentProfileBinding;
 import com.example.qrcheckin.databinding.FragmentViewEventBinding;
@@ -36,12 +40,16 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    int selectPicture = 200;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -68,6 +76,14 @@ public class ProfileFragment extends Fragment {
         } else {
             Log.e(TAG, "User is null");
         }
+
+        binding.editProfilePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+
         binding.profileSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,6 +97,33 @@ public class ProfileFragment extends Fragment {
                     } else {
                         user.setGeo(FALSE);
                     }
+
+                    // Saving the user's profile picture
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                    StorageReference storageReference = storage.getReference();
+                    String imageName = user.getId() + ".jpg";
+                    StorageReference imageRef = storageReference.child("users/" + imageName);
+                    Uri imageUri = Uri.parse(binding.profilePicture.getTag().toString());
+                    UploadTask uploadTask = imageRef.putFile(imageUri);
+
+                    // Monitor the upload progress.
+                    uploadTask.addOnProgressListener(taskSnapshot -> {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                        // This listener will provide the upload status of the file.
+                        // You can add a progress bar in you app and set max of the progress bar to taskSnapshot.getTotalByteCount().
+                        // And process of the progress bar to taskSnapshot.getBytesTransferred().
+                    }).addOnSuccessListener(taskSnapshot -> {
+                        // This listener is triggered when the file is uploaded successfully.
+                        // Using the below code you can get the download url of the file
+                        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                            String imageUrl = uri.toString();
+                            // Do something with the file URL, like saving it to a database or displaying it from cloud.
+                        });
+                    }).addOnFailureListener(exception -> {
+                        // This listener is triggered if there was an exception occured while uploading the file.
+                        // You can display a error message from here if exception occurs.
+                    });
+
                     //Accessing the users database
                     FirebaseFirestore db = FirebaseFirestore.getInstance();
                     CollectionReference usersRef = db.collection("users");
@@ -91,6 +134,7 @@ public class ProfileFragment extends Fragment {
                     data.put("phone", user.getPhone());
                     data.put("homepage", user.getHomepage());
                     data.put("geo", user.isGeo());
+                    data.put("imageRef", user.getImageRef());
                     docRef.update(data)
                             .addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
@@ -110,6 +154,36 @@ public class ProfileFragment extends Fragment {
         return root;
     }
 
+    void imageChooser() {
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), selectPicture);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == selectPicture) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    binding.profilePicture.setImageURI(selectedImageUri);
+                    binding.profilePicture.setTag(selectedImageUri.toString());
+                }
+            }
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -122,7 +196,6 @@ public class ProfileFragment extends Fragment {
         User user = ((QRCheckInApplication) requireActivity().getApplication()).getCurrentUser();
         if (!(binding.profileName.getText().toString().equals(user.getName()))) {
             binding.profileName.setText(user.getName());
-
         }
         if (!(binding.profilePhoneNumber.getText().toString().equals(user.getPhone()))) {
             binding.profilePhoneNumber.setText(user.getPhone());
