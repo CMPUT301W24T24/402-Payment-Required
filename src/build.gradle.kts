465bbc55ca79b6d22dae4a8c8637fcd93cc15d16
Zumzu <74483517+Zumzu@users.noreply.github.com>
2024-04-01 15:04:41 -0600

    Removed weird gradle comment

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -8,11 +8,3 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
-
-//allprojects {
-//    repositories {
-//        google()
-//        jcenter()
-//        maven{url}
-//    }
-//}
