68fd7c8e1714cee30ef33641578790b264704a7c
Tina-Nguyen3004 <ngocthuy@ualberta.ca>
2024-04-04 12:17:51 -0600

    try to write and initialize the event and user
    the user and event initialize for the unit test

--- src/app/src/test/java/com/example/qrcheckin/EventTest.java
+++ src/app/src/test/java/com/example/qrcheckin/EventTest.java
@@ -3,6 +3,7 @@ package com.example.qrcheckin;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.example.qrcheckin.core.Database;
 import com.example.qrcheckin.core.Event;
 import com.example.qrcheckin.core.User;
 import com.example.qrcheckin.core.UserList;
@@ -12,26 +13,12 @@ import org.junit.Test;
 import java.util.Date;
 
 public class EventTest {
-    private Event mockEvent() {
-        User user = new User("123", "Tina", "ngocthuy@gmail.com", "12344555", "123", true, false);
-        UserList list = new UserList();
-        list.add(user);
-        Event testEvent = new Event("1", user, "TestEvent", "TestDescription", "TestRef", new Date(), "TestLocation", 0.0, 0.0, "Test", "Test", "Test", "Test", true, 10, list);
-        return testEvent;
-    }
+    Database db = new Database();
+    User currentUser = new User("e1sXj3vqg6x6qVXgK8tX", "Tina", "", "", "", true, true);
+    ((QRCheckInApplication) getApplicationContext()).setCurrentUser(currentUser);
+    Event mockEvent = new Event(currentUser, "Event for unit test", "", "", new Date(), "",null, null, "", "", "", "", true, 100);
+    db.addEvent(mockEvent);
 
-    //TODO: add a valid thing in the database to check because database doesn't have the database
-    @Test
-    public void checkInTest() {
-        Event event = mockEvent();
-        User user = new User("123", "Tina", "ngocthuy@gmail.com", "12344555", "123", true, false);
-        assertTrue(event.checkIn(user, 0.0, 0.0));
-    }
 
-    @Test
-    public void haversineTest() {
-        Event event = mockEvent();
-        User user = new User("123", "Tina", "ngocthuy@gmail.com", "12344555", "123", true, false);
-        assertEquals(0.0, event.haversine());
-    }
+
 }
